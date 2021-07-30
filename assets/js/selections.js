737e96630465b2b435dfdf62a1da5babb0815607
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-07-30 17:27:24 -0400

    filling the genre drop down

--- assets/js/selections.js
+++ assets/js/selections.js
@@ -7,67 +7,12 @@ const movieAPIHost = "imdb8.p.rapidapi.com";
 // again, just trying to prevent typos
 const movieAPISearch = "https://imdb8.p.rapidapi.com/title/";
 
-
 var movieGenres = []; // make this global so we only have to fill it once
-
-var getPopularMoviesByGenre = function(theGenre, howManyMovies)
-{
-    var tempGenre = theGenre.replace(/\//g,"%2");
-    console.log(tempGenre);
-    var fetchStr = movieAPISearch + "get-popular-movies-by-genre?genre=" + theGenre;
-    fetch(fetchStr, {
-    	"method": "GET",
-	    "headers": {
-	    	"x-rapidapi-key": movieAPIKey,
-	    	"x-rapidapi-host": movieAPIHost 
-	    }
-    })
-    .then(response => {
-	    return response.json();
-     })
-    .then(response => {
-        console.log(response);
-        /* this gives a 100 element array:
-            0: "/title/tt3554046/"
-            1: "/title/tt3480822/"
-            2: "/title/tt1160419/"
-            3: "/title/tt8368408/"
-            etc.
-        */
-        // We need to return an array with howManyMovies movie titles, release date, and plot summaries
-        // first... let's try to get one movie title
-/*
-        Getting the popular movies by genre return a list of 100 movies. We can use that list to get details on each movie, including
-        the title, release date and plot summary.
-        There does not seem to be a fetch string to get a list of movies within a particular genre within a particular time frame.
-        We can get the details of each movie that was returned, but that's 100 calls to the API. IS THERE A WAY TO GET MOVIE DETAILS
-        FOR 100 MOVIES WITH ONE CALL? Even if that exists, we are not guaranteed that the 100 movies will contain 5 movies 
-        within the time frame we want...
-
-        "url": "https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=tt0944947&currentCountry=US",
-        
-        returns an object with:
-                title.title is a string (yes, title twice - the first title is an object with a few elements
-                releaseDate is a string formatted "YYYY-MM-DD"
-                plotSummary.text is a string with the movie description
-*/
-
-    })
-}
-
-// getPopularMoviesByGenre("/chart/popular/genre/adventure");
-
-/*
-https://www.thecocktaildb.com/api.php
-*/
 
 var fillGenreDropDown = function(theMovieGenres)
 {
-    var genreDropDownEl = document.createElement("select");
-    var genreDropDownButton = document.querySelector("#genre-list");
-    genreDropDownButton.replaceWith(genreDropDownEl);
+    var genreDropDownEl = document.querySelector("#genre-list");
     genreDropDownEl.name = "Genres";
-    genreDropDownEl.id = "Genres"
  
     for (var i = 0; i < theMovieGenres.length; i++)
     {
@@ -76,12 +21,6 @@ var fillGenreDropDown = function(theMovieGenres)
         optionEl.text = theMovieGenres[i].description;
         genreDropDownEl.appendChild(optionEl);
     }
- 
-    var labelEl = document.createElement("label");
-    labelEl.innerHTML = "Choose your Genre: "
-    labelEl.htmlFor = "Genre";
- 
-    // document.getElementById("container").appendChild(labelEl).appendChild(genreDropDownEl);
 }
 
 var getMovieGenres = function()
@@ -89,6 +28,7 @@ var getMovieGenres = function()
     movieGenres = [];
     // to prevent too many api calls, we are hardcoding the genre list based on
     // the actual values returned from the code below during testing
+    // CHANGE THIS BACK TO USING DYNAMIC CODE BEFORE WE SUBMIT OUR PROJECT!!!
     movieGenres = [
         {description: "Action", endpoint: "/chart/popular/genre/action"},
         {description: "Adventure", endpoint: "/chart/popular/genre/adventure"},
@@ -116,7 +56,7 @@ var getMovieGenres = function()
     fillGenreDropDown(movieGenres);
 
 
-/* this code works
+/* this code works - CHANGE BACK TO USING THIS CODE BEFORE SUBMITTING OUR PROJECT
     var fetchStr = movieAPISearch + "list-popular-genres";
     fetch(fetchStr, {
     	"method": "GET",
@@ -133,30 +73,6 @@ var getMovieGenres = function()
         fillGenreDropDown(movieGenres);
     })
 */    
-
-    /*
-    Results below. We can use this to hard code the genres so that we are using fewer calls to the API
-0: {description: "Action", endpoint: "/chart/popular/genre/action"}
-1: {description: "Adventure", endpoint: "/chart/popular/genre/adventure"}
-2: {description: "Animation", endpoint: "/chart/popular/genre/animation"}
-3: {description: "Biography", endpoint: "/chart/popular/genre/biography"}
-4: {description: "Comedy", endpoint: "/chart/popular/genre/comedy"}
-5: {description: "Crime", endpoint: "/chart/popular/genre/crime"}
-6: {description: "Documentary", endpoint: "/chart/popular/genre/documentary"}
-7: {description: "Drama", endpoint: "/chart/popular/genre/drama"}
-8: {description: "Family", endpoint: "/chart/popular/genre/family"}
-9: {description: "Fantasy", endpoint: "/chart/popular/genre/fantasy"}
-10: {description: "Film-Noir", endpoint: "/chart/popular/genre/film_noir"}
-11: {description: "History", endpoint: "/chart/popular/genre/history"}
-12: {description: "Horror", endpoint: "/chart/popular/genre/horror"}
-13: {description: "Music", endpoint: "/chart/popular/genre/music"}
-14: {description: "Musical", endpoint: "/chart/popular/genre/musical"}
-15: {description: "Mystery", endpoint: "/chart/popular/genre/mystery"}
-16: {description: "Romance", endpoint: "/chart/popular/genre/romance"}
-17: {description: "Sci-Fi", endpoint: "/chart/popular/genre/sci_fi"}
-18: {description: "Sport", endpoint: "/chart/popular/genre/sport"}
-19: {description: "Thriller", endpoint: "/chart/popular/genre/thriller"}
-20: {description: "War", endpoint: "/chart/popular/genre/war"}
-21: {description: "Western", endpoint: "/chart/popular/genre/western"}
-*/
 }
+
+getMovieGenres();
