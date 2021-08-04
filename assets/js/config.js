f669c53031c6acb90a537ff10be77f6136280407
Ven Kim <venvex@gmail.com>
2021-08-04 00:04:01 -0400

    created drinks api and added config for organization

---
+++ assets/js/config.js
@@ -0,0 +1,41 @@
+// Have the keys as variables just in case we need to switch keys because we've used one too much
+const movieAPIKey = "253de9b7b6mshb2fcda4c7ef7dd1p124ca2jsn8512f3a480a6";
+
+// We probably won't be changing the host, but this will help avoid typos
+const movieAPIHost = "imdb8.p.rapidapi.com";
+
+// again, just trying to prevent typos
+const movieAPISearch = "https://imdb8.p.rapidapi.com/title/";
+
+const numMoviesToDisplay = 5;
+
+
+
+const drinkAPIHost = "https://www.thecocktaildb.com/api.php";
+const drinkAPIsearch = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
+const numDrinksDisplay = 5;
+
+const drinksByGenre = {
+    "action": 'tequila',
+    "adventure": 'tequila',
+    "fantasy": 'tequila',
+    "animation": 'rum',
+    "comedy": 'rum',
+    "family": 'rum',
+    "biography": 'gin',
+    "documentary": 'gin',
+    "history": 'gin',
+    "western": 'gin',
+    "crime": 'whiskey',
+    "drama": 'whiskey',
+    "mystery": 'whiskey',
+    "film-noir": 'whiskey',
+    "romance": 'whiskey',
+    "music": 'vodka',
+    "musical": 'vodka',
+    "horror": 'bourbon',
+    "thriller": 'bourbon',
+    "war": 'bourbon',
+    "sci-fi": 'bourbon',
+}
+
